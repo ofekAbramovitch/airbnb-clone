68e1b12555849190dce5436de5e046b00b927dfd
Ofek Abramovitch <ofek301156@gmail.com>
2023-09-16 23:12:02 +0300

    added stay preview

--- src/pages/home/cmps/stay-preview.tsx
+++ src/pages/home/cmps/stay-preview.tsx
@@ -1,6 +1,7 @@
 import { IStay } from "../../../interfaces/stay-interface"
 import { AiFillStar } from 'react-icons/ai'
 import { stayService } from "../../../services/stay.service"
+import ImgCarousel from "./img-carousel"
 
 interface Props {
     stay: IStay
@@ -14,7 +15,23 @@ export default function StayPreview({ stay, onStay }: Props) {
 
     return (
         <article className="stay-preview" onClick={() => onStay(stay._id, stay.datesForPreview[0], stay.datesForPreview[1])}>
-            
+            <ImgCarousel imgUrls={stay.imgUrls} />
+            <div className="details">
+                <div className="row">
+                    <p className="address">{stay.loc.address}</p>
+                    <div className="stars">
+                        <AiFillStar color="black" />
+                        <p>{rating.toFixed(1)}</p>
+                    </div>
+                </div>
+                <p className="type">{stay.type}</p>
+                <p className="available">
+                    {stayService.formatDateRange(new Date(stay.datesForPreview[0]), new Date(stay.datesForPreview[1]))}
+                </p>
+                <p className="price">
+                    ${stay.price} <span className="prefix">night</span>
+                </p>
+            </div>
         </article>
     )
 }
