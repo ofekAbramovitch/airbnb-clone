947df31198efc267109406366d62b038c56a6da2
Ofek Abramovitch <ofek301156@gmail.com>
2023-09-19 20:40:25 +0300

    stay info header style

---
+++ src/pages/stay/cmps/stay-info/stay-info-bonuses.tsx
@@ -0,0 +1,9 @@
+export default function StayInfoBonuses(){
+    return (
+        <div className="border-bottom stay-info-bonuses ">
+            <div className="row">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
